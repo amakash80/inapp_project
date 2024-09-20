6da71ddbef8d915817fcbafd60f65edf15a94d30
devikas <devikasajith276@gmail.com>
2024-09-20 11:38:32 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+import urllib 
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask import redirect, url_for, flash
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some secret key'
+
+#defining the url string with parameters for mssql db connection
+params = 'DRIVER={ODBC Driver 17 for SQL Server};Server=DESKTOP-FSS96T0\\SQLEXPRESS;DATABASE=course_enquiry;Trusted_Connection=yes;'
+connection_string = urllib.parse.quote_plus(params)
+#add database uri configuration to app.config dictionary
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % connection_string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+mydb_obj = SQLAlchemy(app) #create an SQLAlchemy object
+migrate = Migrate(app, mydb_obj)
 
 # Home route
 @app.route('/')
@@ -17,10 +36,37 @@ def courses():
 def about():
     return render_template('about.html')
 
+# Contacts model
+class contacts(mydb_obj.Model):
+    id = mydb_obj.Column(mydb_obj.Integer, primary_key=True)
+    name = mydb_obj.Column(mydb_obj.String(100))
+    email = mydb_obj.Column(mydb_obj.String(150))
+    message = mydb_obj.Column(mydb_obj.String(500))
+
+# ContactForm
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Your message', validators=[DataRequired()])
+    submit = SubmitField('Send message')
+
 # Contact route
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        # Handle the form submission
+        con_name = form.name.data
+        con_email = form.email.data
+        con_message = form.message.data
+
+        contact = contacts(name=con_name, email=con_email, message=con_message )
+        mydb_obj.session.add(contact)
+        mydb_obj.session.commit()       
+        flash(f'Thank you, {con_name}. Your message has been sent!')
+        return redirect(url_for('contact'))  # Redirect to the same page after submission
+    return render_template('contactus.html', form=form)
 
 # Register route
 @app.route('/register')
@@ -45,3 +91,6 @@ def user_dashboard():
 @app.route('/debug')
 def debug():
     return render_template('debug.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
